d0e8b4f822657167be75284c472553e8852dc860
cuonghxicts <cuong1181998@gmail.com>
2020-12-18 23:52:45 +0700

    networking error

--- src/constraint/index.js
+++ src/constraint/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export const AccessToken = 'accessToken';
 
-export const BackendUrl = 'https://localhost:5000/';
+export const BackendUrl = 'http://localhost:5000';
 
 export const clientId = '751216151427-rekmejfdmcl3tel94ntthgmch5qdont5.apps.googleusercontent.com';

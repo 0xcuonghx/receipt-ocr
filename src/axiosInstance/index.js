fd42768445e317509de2daac78f2fa5cfbc71820
cuonghxicts <cuong1181998@gmail.com>
2021-01-03 17:15:44 +0700

    update

--- src/axiosInstance/index.js
+++ src/axiosInstance/index.js
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 10000
+  timeout: 6000
 });
 
 // do something when request
--- src/constraint/index.js
+++ src/constraint/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export const AccessToken = 'accessToken';
 
-export const BackendUrl = 'http://b40d2b849013.ngrok.io/';
+export const BackendUrl = 'http://172.20.10.12:5000/';
 
 export const clientId = '751216151427-rekmejfdmcl3tel94ntthgmch5qdont5.apps.googleusercontent.com';
